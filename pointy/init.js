25fee8ca5774378f6db5fba1fe5cdc8bb4ed00cf
Andrei Sebastian Cimpean <andreime@mykolab.com>
2015-06-06 15:41:24 +0300

    Add reveal js. Add leap js lib.

---
+++ pointy/init.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var leapMotionScript = document.createElement('script');
+    leapMotionScript.setAttribute('src', 'http://js.leapmotion.com/leap-0.6.3.min.js');
+    document.head.appendChild(leapMotionScript);
+});
